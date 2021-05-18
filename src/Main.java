7566abad646e581371a59aab6d5e2c7351ccf655
Tanzilia Musatova <musatova.tanzilia@gmail.com>
2021-05-18 13:04:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int volue = 500_000_000;
+        int total = balance * volue;
+        System.out.println("Total: " + total);
+    }
+}
