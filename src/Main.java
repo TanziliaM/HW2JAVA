c5ced81868f63bcc73a5d51aafe2de92601b395f
Tanzilia Musatova <musatova.tanzilia@gmail.com>
2021-05-18 14:26:56 +0300

    Commit all

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int balance = 2_000_000_000;
-        int volue = 500_000_000;
-        int total = balance * volue;
+        int balance = 2000000000;
+        System.out.println("balance:" + balance);
+        int volue = 500000000;
+        System.out.println("volue:" + volue);
+        int total = balance + volue;
         System.out.println("Total: " + total);
     }
 }
